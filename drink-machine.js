e3e4c991ab34370428f93d84745c8978ee1d0d56
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-28 13:17:51 +0530

    22 - extending from eventEmitter

---
+++ drink-machine.js
@@ -0,0 +1,6 @@
+class DrinkMachine {
+  serveDrink(size) {
+    if (size === "large") console.log("Serving complementary link");
+  }
+}
+module.exports = DrinkMachine;
---
+++ pizza-shop.js
@@ -0,0 +1,19 @@
+const EventEmitter = require("node:events");
+
+class PizzaShop extends EventEmitter {
+  constructor() {
+    super(); // all previous constructors
+    this.orderNumber = 0;
+  }
+
+  order(size, topping) {
+    this.orderNumber += 1;
+    this.emit("order", size, topping);
+  }
+
+  displayOrderNumber() {
+    console.log(`Current order number: ${this.orderNumber}`);
+  }
+}
+
+module.exports = PizzaShop;
