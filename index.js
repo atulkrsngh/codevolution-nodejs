68910a170d61be572cefe4dd07df832eee279702
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-29 03:14:42 +0530

    fs reading and writing to files

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const buffer = new Buffer.from("Vishwas", "utf-8"); // default is utf-8, no need to pass it
+const fs = require("node:fs");
 
-console.log(buffer.toJSON()); // character code in decimal
-console.log(buffer); // character code in hexadecimal
-console.log(buffer.toString()); // buffer as a string
+// t1 - read a file
+const fileContents = fs.readFileSync("./myFile.txt", "utf-8"); // utf-8 is the default, no need to pass it
+// this (blocking - "sync") is fine if all subsequent ops are dependent on this
+console.log(fileContents);
 
-buffer.write("Code"); // buffers write mode is always 'overwrite'
-console.log(buffer.toString());
+// t2 - read a file, asynchronously
+console.log("First");
+fs.readFile("./myFile.txt", "utf-8", (error, data) => {
+  // "utf-8" is important here, otherwise it'll return the buffer as is
+  // this (error = null, data) is called the error first callback pattern
+  if (error) {
+    console.log(err);
+    return;
+  }
 
-buffer.write("Codevolution"); // buffer discards if it's full
-console.log(buffer.toString()); // 'Codevol' printed 'ution' was discarded
+  console.log("Third - inside")
+  console.log(data);
+});
+console.log("Second")
+// Result - First, Third - inside, Second
+
+// t3 - writing to a file
+fs.writeFileSync("./greet.txt", "Hello, world");
+
+// t4 - writing to a file, asynchronously
+fs.writeFile("./greet.txt", "Hello, world async", (err, data) => {
+  console.log("File written");
+});
+
+// t5 - write in append mode
+fs.writeFileSync("./greet.txt", "Hello, world append", { flag: "a" });
+// or asynchronously
+fs.writeFile(
+  "./greet.txt",
+  "Hello, world async append",
+  { flag: "a" },
+  (err, data) => {
+    console.log("File written");
+  }
+);
