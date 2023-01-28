1e3a6dc9307f5b4c0e3a2aee09beb99bce287a6f
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-29 04:09:07 +0530

    fs module with promises

--- index.js
+++ index.js
@@ -1,42 +1,68 @@
-const fs = require("node:fs");
+const fs = require("node:fs/promises");
 
-// t1 - read a file
-const fileContents = fs.readFileSync("./myFile.txt", "utf-8"); // utf-8 is the default, no need to pass it
-// this (blocking - "sync") is fine if all subsequent ops are dependent on this
-console.log(fileContents);
-
-// t2 - read a file, asynchronously
 console.log("First");
-fs.readFile("./myFile.txt", "utf-8", (error, data) => {
-  // "utf-8" is important here, otherwise it'll return the buffer as is
-  // this (error = null, data) is called the error first callback pattern
-  if (error) {
+// read file with promises
+const fileContentsPromise = fs.readFile("./myFile.txt", "UTF-8");
+fileContentsPromise
+  .then((fileContents) => {
+    console.log(fileContents);
+  })
+  .catch((err) => {
     console.log(err);
-    return;
+  })
+  .finally(() => {
+    console.log("Third");
+  });
+
+console.log("Second");
+// write to file with promises
+const writeFilePromise = fs.writeFile("./greet.txt", "Hello, world");
+writeFilePromise
+  .catch((err) => {
+    console.log(err);
+  })
+  .then(() => {
+    console.log("no errors");
+  });
+
+// read file with async-await
+(async () => {
+  try {
+    const fileContents = await fs.readFile("./myFile.txt", "UTF-8");
+    console.log(fileContents);
+  } catch (err) {
+    console.log(err);
   }
+})();
 
-  console.log("Third - inside")
-  console.log(data);
-});
-console.log("Second")
-// Result - First, Third - inside, Second
-
-// t3 - writing to a file
-fs.writeFileSync("./greet.txt", "Hello, world");
-
-// t4 - writing to a file, asynchronously
-fs.writeFile("./greet.txt", "Hello, world async", (err, data) => {
-  console.log("File written");
-});
-
-// t5 - write in append mode
-fs.writeFileSync("./greet.txt", "Hello, world append", { flag: "a" });
-// or asynchronously
-fs.writeFile(
-  "./greet.txt",
-  "Hello, world async append",
-  { flag: "a" },
-  (err, data) => {
-    console.log("File written");
+// write to file with async-await
+(async () => {
+  try {
+    await fs.writeFile("./greet.txt", "Hello, world");
+    console.log("no errors");
+  } catch (err) {
+    console.log(err);
   }
-);
+})();
+
+// write to file, append mode, with promises
+const writeFilePromiseAppendMode = fs.writeFile("./greet.txt", "Hello, world", {
+  flag: "a",
+});
+writeFilePromiseAppendMode
+  .catch((err) => {
+    console.log(err);
+  })
+  .then(() => {
+    console.log("no errors");
+  });
+
+// write to file, append mode, with async-await
+(async () => {
+  try {
+    await fs.writeFile("./greet.txt", "Hello, world", { flag: "a" });
+    console.log("no errors");
+  } catch (err) {
+    console.log(err);
+  }
+})();
