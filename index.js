7baf4f91356b1ea9f44cd8036c647b7b5b96ee0c
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-28 22:58:02 +0530

    Using different variables to store the same function (literal-ly) doesn't work, since they have different address

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const EventEmitter = require("node:events");
 
 const emitter = new EventEmitter();
 
-emitter.addListener("order-pizza", () => console.log("Pizza ordered!"));
-emitter.removeListener("order-pizza", () => console.log("Pizza ordered!"));
+const callback1 = () => console.log("Pizza ordered!");
+const callback2 = () => console.log("Pizza ordered!");
+
+emitter.addListener("order-pizza", callback1);
+emitter.removeListener("order-pizza", callback2);
 
 emitter.emit("order-pizza");
 
-// Result: listener not removed
+// Result: listener not removed, since functions cannot be compared.
+// Removal worked previously by comparing the reference (memory address). Here the two have different address.
