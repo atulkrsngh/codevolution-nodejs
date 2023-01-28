c5623674cad1bdb19fbca5e00dc31bf22c703166
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-29 00:56:47 +0530

    Creating and writing text to buffer

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const EventEmitter = require("node:events");
+const buffer = new Buffer.from("Vishwas", "utf-8"); // default is utf-8, no need to pass it
 
-const emitter = new EventEmitter();
-
-const callback1 = () => console.log("Pizza ordered!");
-const callback2 = () => console.log("Pizza ordered!");
-
-emitter.addListener("order-pizza", callback1);
-emitter.removeListener("order-pizza", callback2);
-
-emitter.emit("order-pizza");
-
-// Result: listener not removed, since functions cannot be compared.
-// Removal worked previously by comparing the reference (memory address). Here the two have different address.
+console.log(buffer.toJSON());   // character code in decimal
+console.log(buffer);            // character code in hexadecimal
+console.log(buffer.toString()); // buffer as a string
