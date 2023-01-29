ef9cb9b25dc2ae09e63aedcad4ecbc4afa97fc42
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-29 22:29:24 +0530

    Demo piping

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const fs = require("node:fs");
+const zlib = require("node:zlib"); // compress data using gzib
 
 const readableStream = fs.createReadStream("./file.txt", {
   encoding: "utf-8",
   highWaterMark: 2,
 });
 
-const writeableStream = fs.createWriteStream("./file2.txt");
-
-// let count = 0;
-// readableStream.on("data", (chunk) => {
-//   writeableStream.write(`${count} ${chunk}\n`);
-//   count += 1;
-// });
-
-// simpler, less control but simpler
-readableStream.pipe(writeableStream);
+const gzip = zlib.createGzip();
+readableStream.pipe(gzip).pipe(fs.WriteStream("./file2.txt.gz"));
