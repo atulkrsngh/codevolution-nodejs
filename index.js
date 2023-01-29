089f7f8b64123ed80746eefc487a9af2c563ae64
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-29 23:21:01 +0530

    Demo http server

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("node:http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200); // status code
+  res.end("Hello world");
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+// Browse http://localhost:3000 (or http://127.0.0.0:3000)
