b7bed560a8a27b38e95d9c9d30be5bd363e925e3
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-29 19:51:16 +0530

    Demo setting max capacity for stream

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const fs = require("node:fs");
 
-const readableStream = fs.createReadStream("./file.txt", { encoding: "utf-8" });
+const readableStream = fs.createReadStream("./file.txt", {
+  encoding: "utf-8",
+  highWaterMark: 2,
+});
 
 const writeableStream = fs.createWriteStream("./file2.txt");
 
+let count = 0;
 readableStream.on("data", (chunk) => {
-  writeableStream.write(chunk);
+  writeableStream.write(`${count} ${chunk}\n`);
+  count += 1;
 });
