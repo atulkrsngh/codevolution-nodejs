aa3b6efc0d77d1911c5b0852d8bc35ae410dad46
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-01-30 13:50:58 +0530

    Send HTML response by reading file async - callback

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fs = require("node:fs");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
 
-  const htmlFileContents = fs.readFileSync("./index.html", "utf-8");
-  res.end(htmlFileContents);
+  // const htmlFileContents = fs.readFileSync("./index.html", "utf-8");
+  // res.end(htmlFileContents);
+
+  // async approach - callback
+  fs.readFile("./index.html", "utf-8", (err, htmlFileContents) => {
+    if (!err) res.end(htmlFileContents);
+  });
 });
 
 server.listen(3000, () => {
