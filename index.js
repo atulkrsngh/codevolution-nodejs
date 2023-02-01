dcfd578a0d3b779a8e4b0f4a756031fe028f547d
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-01 22:50:02 +0530

    Sync call - time proportial to number of calls

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const fs = require("node:fs");
+const crypto = require("node:crypto");
 
-console.log("First");
-
-fs.readFile("./file.txt", "utf-8", (err, data) => {
-  console.log("File contents");
-});
-
-console.log("Last");
+const start = Date.now();
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+crypto.pbkdf2Sync("password", "salt", 100000, 512, "sha512");
+console.log("Hash: ", Date.now() - start);
