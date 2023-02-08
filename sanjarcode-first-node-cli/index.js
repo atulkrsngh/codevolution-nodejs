b547c1666fea7e87a10ce8194242a6df2c13aa52
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-09 03:34:02 +0530

    Use yargs

--- sanjarcode-first-node-cli/index.js
+++ sanjarcode-first-node-cli/index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
+const yargs = require("yargs");
+const myOptions = yargs(process.argv).argv;
+
 const printFiveMoves = async (pokemonName) => {
   const response = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
   );
+
+  if (!response.ok) {
+    console.log(response.statusText);
+    return;
+  }
+
   const pokemon = await response.json();
 
   const moves = pokemon?.moves.map(({ move }) => move.name);
@@ -11,5 +20,5 @@ const printFiveMoves = async (pokemonName) => {
   console.log(moves.slice(0, 5));
 };
 
-const pokemonName = process.argv[2] ?? "charmander";
+const pokemonName = myOptions.pokemon;
 printFiveMoves(pokemonName);
