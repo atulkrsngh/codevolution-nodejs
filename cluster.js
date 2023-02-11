0bc07a91b3c124d4b1ca1a41046183920f81ffee
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-11 17:51:07 +0530

    Demo cluster code

---
+++ cluster.js
@@ -0,0 +1,24 @@
+const cluster = require("node:cluster");
+const http = require("node:http");
+
+if (cluster.isMaster) {
+  console.log(`Master running with pid ${process.pid}`);
+  cluster.fork();
+  cluster.fork();
+} else {
+  const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+
+    if (req.url === "/") res.end("Home page");
+    else if (req.url === "/slow-page") {
+      for (let i = 0; i < 6e9; i++) {} // slow op
+      res.end("Slow page");
+    }
+  });
+
+  server.listen(8000, () =>
+    console.log(
+      `Worker running with pid ${process.pid} on port ${server.address().port}`
+    )
+  );
+}
