2d1581c0310e981473276bf18c1cec2c0e460544
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-02-13 04:17:43 +0530

    Basic server returning JSON for easy experimentation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+const http = require("node:http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify("Hello from render.com server"));
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
