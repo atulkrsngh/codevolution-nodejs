09066230d4f4fe30407b81957267ad0d08a26f39
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-28 00:23:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require("node:fs");
 
-const readableStream = fs.createReadStream("./file.txt", {
+const readableStream = fs.createReadStream("./file.txt", { // read stream to read data in chunks from file.txt
   encoding: "utf-8",
-  highWaterMark: 2,
+  highWaterMark: 2, // data in chunks of 2 bytes, it's an optional
 });
 
-const writeableStream = fs.createWriteStream("./file2.txt", { flags: "a" });
+const writeableStream = fs.createWriteStream("./file2.txt", { flags: "a" }); // write stream to write data in chunks to file2.txt
 // append mode
 
 let count = 0;
-readableStream.on("data", (chunk) => {
+readableStream.on("data", (chunk) => { // adding data event to the readstream
   writeableStream.write(`${count} ${chunk}\n`);
   count += 1;
 });
