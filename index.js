a4314182ff1a87c57b4a7392704a27e776ca3df8
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-27 23:39:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const buffer = new Buffer.from("Vishwas", "utf-8"); // default is utf-8, no need to pass it
+const buffer = new Buffer.from("Vishwas", "utf-8"); // global feature, no need to import anything. default is utf-8, no need to pass it
 
-console.log(buffer.toJSON()); // character code in decimal
+console.log(buffer.toJSON()); // character code in decimal (86, 105, 115, 104, 119, 97, 115)
 console.log(buffer); // character code in hexadecimal
-console.log(buffer.toString()); // buffer as a string
+console.log(buffer.toString()); // buffer as a string which is Voshwas itself
 
 buffer.write("Code"); // buffers write mode is always 'overwrite'
-console.log(buffer.toString());
+console.log(buffer.toString()); // Codewas
 
 buffer.write("Codevolution"); // buffer discards if it's full
 console.log(buffer.toString()); // 'Codevol' printed 'ution' was discarded
