10cd1dbdb0039ed72473019a1b54f9b819383d1c
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-28 12:56:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 const http = require("node:http");
 const fs = require("node:fs");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer( (req, res) => {
   if (req.url === "/") { // sending html content
-    res.writeHead(200, { "Content-Type": "text/html" }); // without content type, full string will be sent in response
+    res.writeHead(200, { "Content-Type": "text/html" }); // without content type text/html, full string will be sent in response
     return res.end(
       "<html><head><title>Home</title></head><body>Home page</body></html>"
     );
+
   } else if (req.url === "/about") { // sending html from a separate file
     res.writeHead(200, { "Content-Type": "text/html" });
     //const html = fs.readFileSync("./about.html", "utf-8"); // avoid reading complete file synchronously
     //return res.end(html);
     fs.createReadStream("./about.html").pipe(res); // better way than above 2 lines to read html or any file
-  } else if (req.url === "/api") {
+  
+  } else if (req.url === "/dynamic") { // sending dynamic html from a separate file
+    const name = "Atul";
+    res.writeHead(200, { "Content-Type": "text/html" });
+    let html = fs.readFileSync("./index.html", "utf-8"); // avoid reading complete file synchronously
+    html.replace("{{name}}", name); // replacing name with Atul
+    return res.end(html);
+  
+  } else if (req.url === "/api") { // sending json
     res.writeHead(200, { "Content-Type": "application/json" });
     const superHero = {
       firstName: "Bruce",
       lastName: "Wayne",
     };
     return res.end(JSON.stringify(superHero)); // we can't send javascript object as is in response
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
+  
+  } else { // sending simple plain string
+    res.writeHead(404, { "Content-Type": "text/plain" });
     return res.end(
-      "<html><head><title>404</title></head><body>Page not found</body></html>"
+      "Page Not found"
     );
   }
 });
