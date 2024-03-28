0fc6bc750e4958bca6326d9ca723fcb7c8c03051
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-29 01:51:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,16 @@ setTimeout(() => {
   console.log("This is setTimeout 1");
 }, 0)
 
+setImmediatek(() => {
+  console.log("This is setImmediate 1");
+})
+
 for(let i = 0; i < 20000000000; i++) {} // This line will ensure that setTimeout timer elapses when event loop goes to timer queue for callback check
 
 /*
   This is process nexttick 1
   This is promise resolve 1
   This is setTimeout 1
+  This is setImmediate 1 (I/O events are polled and callback functions are added to the I/O queue only after the I/O operation is completed)
   This is readfile 1
 */
