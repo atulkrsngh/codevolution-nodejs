43044f9e35963d5cfc458502b2ca90ceddc3b03d
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-29 01:59:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const fs = require("node:fs");
 
 fs.readFile(__filename, () => {
   console.log("This is readfile 1");
+  setImmediate(() => {
+    console.log("This is setImmediate inside readfile");
+  })
+
+  process.nextTick(() => {
+    console.log("This is process nexttick inside readFile");
+  })
+  
+  Promise.resolve().then(() => {
+    console.log("This is promise resolve inside readFile");
+  })
 })
 
 process.nextTick(() => {
@@ -16,16 +27,14 @@ setTimeout(() => {
   console.log("This is setTimeout 1");
 }, 0)
 
-setImmediatek(() => {
-  console.log("This is setImmediate 1");
-})
-
 for(let i = 0; i < 20000000000; i++) {} // This line will ensure that setTimeout timer elapses when event loop goes to timer queue for callback check
 
 /*
   This is process nexttick 1
   This is promise resolve 1
   This is setTimeout 1
-  This is setImmediate 1 (I/O events are polled and callback functions are added to the I/O queue only after the I/O operation is completed)
   This is readfile 1
+  This is process nexttick inside readFile
+  This is promise resolve inside readFile
+  This is setImmediate inside readfile
 */
