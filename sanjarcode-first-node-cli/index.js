3e20d2b844c6de854607d7a4f588df03fb5ea2f2
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-29 22:15:52 +0530

    Update index.js

--- sanjarcode-first-node-cli/index.js
+++ sanjarcode-first-node-cli/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node // this is to tell operating system which interpreter to use, node in this case which is installed at path /usr/bin/env
 
-const inquirer = require("inquirer");
+const inquirer = require("inquirer"); // It is used to make interactive CLI
 
 const printFiveMoves = async (pokemonName) => {
   const response = await fetch(
@@ -20,13 +20,15 @@ const printFiveMoves = async (pokemonName) => {
 };
 
 const prompt = inquirer.createPromptModule();
-prompt([
+prompt([ // this contains array of questions
   {
-    type: "input",
+    type: "input", 
     name: "pokemon",
     message: "Enter a pokemon name to view its first 5 moves",
   },
 ]).then((answerObj) => {
-  const pokemonName = answerObj.pokemon;
+  const pokemonName = answerObj.pokemon; // here pokeman comes from name in prompt
   printFiveMoves(pokemonName);
 });
+
+// When we run the command sanjarcode-pokedex, it will ask the above question and whatever we enter will go to pokemonName passed in printFiveMoves function
