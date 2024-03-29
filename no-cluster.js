9b1b511f405355e45aea2ca13baa13bc3ea5f6e8
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-29 22:30:14 +0530

    Update no-cluster.js

--- no-cluster.js
+++ no-cluster.js
@@ -5,9 +5,11 @@ const server = http.createServer((req, res) => {
 
   if (req.url === "/") res.end("Home page");
   else if (req.url === "/slow-page") {
-    for (let i = 0; i < 6e9; i++) {} // slow op
+    for (let i = 0; i < 6e9; i++) {} // simulating slow operation
     res.end("Slow page");
   }
 });
 
 server.listen(8000, () => console.log("Server is running on port 8000"));
+
+// Here, if we load slow-page first and then Home Page, Home page will also takes time to get loaded because the main thread is blocked in loading slow-page and it won't load Home-page meanwhile
