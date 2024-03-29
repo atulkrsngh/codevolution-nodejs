f52de2eb1e1b1c4b870a25acbea8f6fd832a0ee5
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-03-29 23:03:54 +0530

    Update main-thread.js

--- main-thread.js
+++ main-thread.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") res.end(JSON.stringify("Home page"));
   else if (req.url === "/slow-page") {
     const worker = new Worker("./worker-thread.js"); // creating new worker thread  passing worker file as argument
-    worker.on("message", (j) => {
+    worker.on("message", (j) => { // listening using message event. j is passed here from worker thread
       res.end(JSON.stringify(`Slow page ${j}`));
     });
   }
