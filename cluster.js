80592c1a72ebe52e4aab7d505dee7d201a3869a1
Atul Kumar <33157076+atulkrsngh@users.noreply.github.com>
2024-08-20 19:09:34 +0530

    Update cluster.js

--- cluster.js
+++ cluster.js
@@ -23,6 +23,8 @@ if (cluster.isMaster) { // true if file is treated as master
   );
 }
 // Here if we load slow-page first followed by home page, home page load will not be blocked because of slow-page. It will get loaded in worker separately. two worker will be used for loading 2 pages
-
+/* the worker assignment is dynamic and handled by the Node.js cluster module, which distributes incoming requests 
+among the workers. This ensures that heavy operations like /slow-page do not block the processing of other requests 
+like /. The round-robin load balancing and the independent nature of worker processes help achieve this. */
 const os = require("node:os");
 console.log(os.cpus().length); // check number of CPU cores
